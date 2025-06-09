b5e4d7df34290330fa245c5b853f69b2d36287d9
Amanbundela75 <aman75bundela@gmail.com>
2025-06-09 21:10:51 +0530

    Updated admin.js code.

--- 3rd Year Projects/LMS-Project/frontend/src/api/admin.js
+++ 3rd Year Projects/LMS-Project/frontend/src/api/admin.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL + '/admin.js';
+const API_URL = import.meta.env.VITE_API_URL + '/admin';
 
 // Admin User Management
 export const fetchAllUsersAdmin = async (token) => {
