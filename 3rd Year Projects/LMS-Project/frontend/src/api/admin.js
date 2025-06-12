801e026ec3ccfc92694ad944b0191365d8c3b78f
Amanbundela75 <aman75bundela@gmail.com>
2025-06-12 20:49:59 +0530

    code Updated.

--- 3rd Year Projects/LMS-Project/frontend/src/api/admin.js
+++ 3rd Year Projects/LMS-Project/frontend/src/api/admin.js
@@ -1,27 +1,26 @@
-import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL + '/admin';
+import apiClient from './axiosConfig';
 
 // Admin User Management
 export const fetchAllUsersAdmin = async (token) => {
     const config = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await axios.get(`${API_URL}/users`, config);
+    const response = await apiClient.get(`/users`, config);
     return response.data.data;
 };
 
 export const fetchUserByIdAdmin = async (userId, token) => {
     const config = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await axios.get(`${API_URL}/users/${userId}`, config);
+    const response = await apiClient.get(`/users/${userId}`, config);
     return response.data.data;
 };
 
 export const updateUserByAdminApi = async (userId, userData, token) => {
     const config = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await axios.put(`${API_URL}/users/${userId}`, userData, config);
+    const response = await apiClient.put(`/users/${userId}`, userData, config);
     return response.data;
 };
 
 export const deleteUserByAdminApi = async (userId, token) => {
     const config = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await axios.delete(`${API_URL}/users/${userId}`, config);
+    const response = await apiClient.delete(`/users/${userId}`, config);
     return response.data;
 };
